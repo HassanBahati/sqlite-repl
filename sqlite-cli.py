1ced18ea98a2761bc17c7144af904bf59a485f66
bahati.mukisa <bahati.mukisa@outbox.co.ug>
2022-05-08 13:54:54 +0300

    add auto complete

--- sqlite-cli.py
+++ sqlite-cli.py
@@ -1,19 +1,42 @@
 from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.lexers import PygmentsLexer
+from pygments.lexers.sql import SqlLexer
+
+sql_completer = WordCompleter([
+    'abort', 'action', 'add', 'after', 'all', 'alter', 'analyze', 'and',
+    'as', 'asc', 'attach', 'autoincrement', 'before', 'begin', 'between',
+    'by', 'cascade', 'case', 'cast', 'check', 'collate', 'column',
+    'commit', 'conflict', 'constraint', 'create', 'cross', 'current_date',
+    'current_time', 'current_timestamp', 'database', 'default',
+    'deferrable', 'deferred', 'delete', 'desc', 'detach', 'distinct',
+    'drop', 'each', 'else', 'end', 'escape', 'except', 'exclusive',
+    'exists', 'explain', 'fail', 'for', 'foreign', 'from', 'full', 'glob',
+    'group', 'having', 'if', 'ignore', 'immediate', 'in', 'index',
+    'indexed', 'initially', 'inner', 'insert', 'instead', 'intersect',
+    'into', 'is', 'isnull', 'join', 'key', 'left', 'like', 'limit',
+    'match', 'natural', 'no', 'not', 'notnull', 'null', 'of', 'offset',
+    'on', 'or', 'order', 'outer', 'plan', 'pragma', 'primary', 'query',
+    'raise', 'recursive', 'references', 'regexp', 'reindex', 'release',
+    'rename', 'replace', 'restrict', 'right', 'rollback', 'row',
+    'savepoint', 'select', 'set', 'table', 'temp', 'temporary', 'then',
+    'to', 'transaction', 'trigger', 'union', 'unique', 'update', 'using',
+    'vacuum', 'values', 'view', 'virtual', 'when', 'where', 'with',
+    'without'], ignore_case=True)
 
 def main():
-    session = PromptSession()
+    session= PromptSession(lexer=PygmentsLexer(SqlLexer), completer=sql_completer)
 
     while True:
-        # prompt user for text input 
         try:
             text = session.prompt('> ')
-        # continue to next line and get more user input when ctrlC has been pressed
         except KeyboardInterrupt:
             continue
-        # exit the terminal when ctrlD has been pressed
         except EOFError:
-            break
+            break 
         else:
-            print('Goodbye!')
+            print('Your entered: ', text)
+        print('Goodbye!')
+    
 
 main()
